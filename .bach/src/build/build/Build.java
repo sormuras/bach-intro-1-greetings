7eb110bce8d1ecff862386b1a80680970d417594
Christian Stein <sormuras@gmail.com>
2020-07-14 20:26:44 +0200

    Update build program to Bach.java 11.4's API

--- .bach/src/build/build/Build.java
+++ .bach/src/build/build/Build.java
@@ -4,6 +4,6 @@ import de.sormuras.bach.Bach;
 
 class Build {
   public static void main(String... args) {
-    Bach.of(project -> project.withVersion("1-ea")).buildProject();
+    Bach.of(project -> project.name("greetings").version("1-ea")).build();
   }
 }
