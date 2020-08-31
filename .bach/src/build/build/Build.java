ec8db3485e0234254dc5f7fb1674e309b7d568e4
Christian Stein <sormuras@gmail.com>
2020-08-31 08:34:23 +0200

    Upgrade to Bach.java 11.8

--- .bach/src/build/build/Build.java
+++ .bach/src/build/build/Build.java
@@ -2,7 +2,7 @@ package build;
 
 import de.sormuras.bach.Bach;
 
-class Build {
+public class Build {
   public static void main(String... args) {
     Bach.of(project -> project.name("greetings").version("1-ea")).build();
   }
