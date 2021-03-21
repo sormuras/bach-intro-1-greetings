a4a0808e3193e064b4bef8d3fbe072d1f1d13da5
Christian Stein <sormuras@gmail.com>
2021-03-21 11:00:24 +0100

    Upgrade to Bach 17-ea

--- .bach/src/build/build/Build.java
+++
@@ -1,9 +0,0 @@
-package build;
-
-import de.sormuras.bach.Bach;
-
-public class Build {
-  public static void main(String... args) {
-    Bach.of(project -> project.name("greetings").version("1-ea")).build();
-  }
-}
